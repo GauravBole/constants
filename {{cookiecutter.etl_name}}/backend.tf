22c00741a99abf017b834c28080540fbb46257c9
gauravbole <gauravbole94@gmail.com>
2022-12-18 22:02:31 +0530

    dynamic region name error

--- {{cookiecutter.etl_name}}/backend.tf
+++ {{cookiecutter.etl_name}}/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "{{cookiecutter.etl_name}}-tf"
     key    = "terraform.tfstate"
     # region = "{%- if not cookiecutter.region_name -%} us-east-1 {%- endif -%}"
-    region = "{{cookiecutter.region_name}}" != "" ? "{{cookiecutter.region_name}}": "us-east-1"
+    region = "{{cookiecutter.region_name}}" != "Enter region name:" ? "{{cookiecutter.region_name}}": "us-east-1"
   }
 }
 
